d195773ecc9bff080072632e05d1cceaf116446a
ENTPRESTIGIOUS <bandcomposing54@gmail.com>
2022-08-05 00:28:58 -0700

    fixed mistakes!

--- q2.sql
+++ q2.sql
@@ -12,26 +12,29 @@ CREATE TABLE Orders(
 
 SELECT Last_Name, First_Name
 FROM Customers
-WHERE City != 'Coquitlam';
+WHERE City != 'Coquitlam'
+ORDER BY Last_Name ASC;
 
 SELECT ProductSerialNumber, Name, Price
 FROM Products
-WHERE Price < 300 ORDER BY Date_Manufactured DESC;
+WHERE Price < 300
+ORDER BY Date_Manufactured DESC;
 
 SELECT Suppliers.Supplier_Name, ProductSerialNumber, Name, Price
 FROM Products, Suppliers
-WHERE (Price >= 500 AND Price <= 1000) AND (Supplier_Country = 'China' OR Supplier_Country = 'Vietnam');
+WHERE (Price >= 500 AND Price <= 1000) AND (Suppliers.Supplier_Name = Products.Supplier_Name AND Suppliers.Supplier_ID = Products.Supplier_ID) AND (Supplier_Country = 'China' OR Supplier_Country = 'Vietnam');
 
-SELECT *
-FROM Orders
-WHERE Shipping_Address = 'MONTREAL' AND Order_cost < 600;
+SELECT Orders.*
+FROM Orders, Customers
+WHERE Shipping_Address = 'MONTREAL' AND Order_cost < 600 AND Customers.Customer_ID = Orders.Customer_ID;
 
 SELECT Supplier_Name, COUNT(ProductSerialNumber), AVG(Price)
-FROM Products;
+FROM Products
+GROUP BY Supplier_Name;
 
 UPDATE Suppliers
 SET Supplier_City = 'Halifax'
 WHERE Supplier_Name = 'Razer Inc.';
 
 DELETE FROM Products
-WHERE ProductSerialNumber = 3443;
+WHERE Supplier_Name = 'MSI Inc.' AND ProductSerialNumber = 3443;
